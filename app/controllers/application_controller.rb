fa8f8155c75ff1f3bea8ac888b14969d36e576af
Olha Gruzglina <olga.zhmura@gmail.com>
2022-03-06 17:06:42 -0500

    commit tree e58395dba0a138ba6c811601f16b812e817ce16a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, 'application/json'
   # add routes
+  get '/bakeries' do 
+    # '<h2>Hello World</h2>'
+    Bakery.all.to_json
+  end
 
+  get '/bakeries/:id' do 
+    Bakery.find(params[:id]).to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do 
+    BakedGood.by_price.to_json
+  end
+
+  get '/baked_goods/most_expensive' do 
+    BakedGood.by_price.first.to_json
+  end
 end
